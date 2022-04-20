050d653903887607a5f2a9c2d688369c61b2e173
YasuY1 <lovesoulmusic@icloud.com>
2022-04-20 13:30:45 +0900

    初期動作完了

--- easy_md_js.js
+++ easy_md_js.js
@@ -3,21 +3,16 @@ const printArea = document.getElementById('printArea');
 const input = document.createElement('input');
 const span = document.createElement('span');
 
-let inputCount;
-
-input.style.width = '6px';
 
 /* ------- 初期動作 -------- */
-markdown.addEventListener('click',serchText);
-// input.addEventListener('input',widthExtender);
+markdown.addEventListener('click',serchText,{once:true});
+input.addEventListener('input',widthExtender);
 
 function serchText(){
     if(printArea.firstChild.textContent === ''){
         printArea.firstElementChild.insertAdjacentElement('beforeend',input);
-        input.style.fontSize = window.getComputedStyle(input.parentElement).getPropertyValue('font-size');
-        input.style.fontWeight = window.getComputedStyle(input.parentElement).getPropertyValue('font-weight');
+        inputStyleChenger();
         input.focus();
-        input.addEventListener('input',widthExtender);
         input.addEventListener('keydown',bOLMaker);//bOL = body of letter
     }
 }
@@ -27,9 +22,15 @@ function widthExtender(){
     span.textContent = input.value;
     span.style.display = 'inline-block';
     span.style.opacity = '0';
-    span.style.fontSize = window.getComputedStyle(input).getPropertyValue('font-size');
-    span.style.fontWeight = window.getComputedStyle(input).getPropertyValue('font-weight');
+    span.style.fontSize = window.getComputedStyle(this.parentElement).getPropertyValue('font-size');
+    span.style.fontWeight = window.getComputedStyle(this.parentElement).getPropertyValue('font-weight');
     input.style.width = window.getComputedStyle(span).getPropertyValue('width');
+}
+
+function inputStyleChenger(){
+    input.style.width = '6px';
+    input.style.fontSize = window.getComputedStyle(input.parentElement).getPropertyValue('font-size');
+    input.style.fontWeight = window.getComputedStyle(input.parentElement).getPropertyValue('font-weight');
 }
 /*-----------------------------*/
 
@@ -40,6 +41,7 @@ function bOLMaker(e){
         input.value = '';
         printArea.insertAdjacentHTML('beforeend','<p>');
         printArea.lastElementChild.insertAdjacentElement('beforeend',input);
+        inputStyleChenger();
         input.focus();
     }
 }
